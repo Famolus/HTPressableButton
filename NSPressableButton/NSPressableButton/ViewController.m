6475da46e1b05781ff15d6ca82f590bab7bacbae
He Rin Kim <me@herinkim.com>
2014-04-14 01:26:28 +0700

    Added title

--- NSPressableButton/NSPressableButton/ViewController.m
+++ NSPressableButton/NSPressableButton/ViewController.m
@@ -18,6 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self setTitle:@"NSPressableButtons"];
+    self.title=@"NSPressableButtons";
 }
 
 - (void)didReceiveMemoryWarning
