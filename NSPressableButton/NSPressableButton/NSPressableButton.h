02a5cb87c2d3c694ffede0d27c64181d21c213d0
He Rin Kim <me@herinkim.com>
2014-04-14 01:19:13 +0700

    Created NSPressableButton file

---
+++ NSPressableButton/NSPressableButton/NSPressableButton.h
@@ -0,0 +1,13 @@
+//
+//  NSPressableButton.h
+//  NSPressableButton
+//
+//  Created by He Rin Kim on 4/14/14.
+//  Copyright (c) 2014 He Rin Kim & Thanakron Tandavas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSPressableButton : UIButton
+
+@end
---
+++ NSPressableButton/NSPressableButton/NSPressableButton.m
@@ -0,0 +1,31 @@
+//
+//  NSPressableButton.m
+//  NSPressableButton
+//
+//  Created by He Rin Kim on 4/14/14.
+//  Copyright (c) 2014 He Rin Kim & Thanakron Tandavas. All rights reserved.
+//
+
+#import "NSPressableButton.h"
+
+@implementation NSPressableButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
