fc39d3e215cd49dba9e8e42139ac2eaaac238fa8
Paul Williamson <squarefrog@gmail.com>
2014-05-10 12:03:43 +0100

    Add ALL the comments

--- Classes/HTPressableButton.h
+++ Classes/HTPressableButton.h
@@ -14,36 +14,109 @@ typedef NS_ENUM(NSInteger, HTPressableButtonStyle) {
     HTPressableButtonStyleCircular
 };
 
+/**
+ The `HTPressableButton` class is designed for iOS developers to be able to spend time developing ideas, not building basic buttons. These stylish and flat-designed buttons can easily be modified and are perfect for almost any project. With no worry over color choice, HTPressableButton also includes beautiful color schemes that perfectly suit with your app.
+ 
+ There are three built-in styles: rectangle, rounded rectangle, and circular.
+ 
+ @warning You *must* specify the frame before choosing a button style
+ 
+ **/
+
 @interface HTPressableButton : UIButton
 
+/**
+ The color of the button
+ **/
 @property(nonatomic, strong, readwrite) UIColor *buttonColor UI_APPEARANCE_SELECTOR;
+
+/**
+ The color of the shadow
+ **/
 @property(nonatomic, strong, readwrite) UIColor *shadowColor UI_APPEARANCE_SELECTOR;
+
+/**
+ The shadow height as a CGFloat
+ **/
 @property(nonatomic, readwrite) CGFloat shadowHeight UI_APPEARANCE_SELECTOR;
+
+/**
+ The corner radius of the button
+ **/
 @property(nonatomic, readwrite) CGFloat cornerRadius UI_APPEARANCE_SELECTOR;
 
 /**
- * Button style
- *
- * @remarks Choose between: square, rounded, or circular
- */
+ Button style
+ 
+ Choose between: square, rounded, or circular
+ @warning Ensure you have set the buttons frame before applying a style
+ **/
 @property(nonatomic, readwrite) HTPressableButtonStyle style;
 
 #pragma mark - Init
+
+/**
+ Initializes and returns a newly allocated button object with the specified frame rectangle.
+ @param frame The frame rectangle for the view, measured in points.
+ **/
 - (id)initWithFrame:(CGRect)frame;
 
 #pragma mark - Set Button Style
+
+/**
+ Set the button's main color
+ @param buttonColor The fill color to be used by the button
+ **/
 -(void) setButtonColor:(UIColor *)buttonColor;
+
+/**
+ Set the button's shadow color
+ @param shadowColor The fill color to be used by the shadow
+ **/
 -(void) setShadowColor:(UIColor *)shadowColor;
+
+/**
+ Set the shadow height
+ @param shadowHeight A float value which sets the height of the shadow
+ **/
 -(void) setShadowHeight:(CGFloat)shadowHeight;
+
+/**
+ Set the button style
+ 
+ Can be HTPressableButtonStyleRect, HTPressableButtonStyleRounded or 
+ HTPressableButtonStyleCircular
+ @param style The display style of the button
+ **/
 -(void) setStyle:(HTPressableButtonStyle) style;
 
 #pragma mark - Set Button Default Style
+
+/**
+ Applies the default `jayColor` to the button
+ **/
 -(void) setDefaultButtonColor;
+
+/**
+ Applies the default `darkJayColor` color to the shadow
+ **/
 -(void) setDefaultShadowColor:(UIColor *)buttonColor;
+
+/**
+ Applies the default shadow height; 17% of the overall button size.
+ **/
 -(void) setDefaultShadowHeight;
+
+/**
+ Override the current highlighted state
+ **/
 -(void) setHighlighted:(BOOL)highlighted;
 
 #pragma mark - Create Button
+
+/**
+ Applies the currently set theme to the button
+ **/
 -(void) createButton;
 
 @end
