e4d1b7f24fc0c5c3cb41a55e5343e401e195e8de
Paul Williamson <squarefrog@gmail.com>
2014-05-19 19:20:03 +0100

    Added a failing test

---
+++ Example/HTPressableButtonTests/XCTests.m
@@ -0,0 +1,34 @@
+//
+//  XCTests.m
+//  HTPressableButton
+//
+//  Created by Paul Williamson on 19/05/2014.
+//  Copyright (c) 2014 He Rin Kim & Thanakron Tandavas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XCTests : XCTestCase
+
+@end
+
+@implementation XCTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
