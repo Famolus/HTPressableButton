bcf1c6722cdd206a4c40b4213d2ba8a05b31a5e4
tandavas <thanakron@tandavas.com>
2014-09-20 02:05:48 +0700

    Fixed build setup

---
+++ Example/Pods/Pods-HTPressableButtonTests-FBSnapshotTestCase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HTPressableButtonTests_FBSnapshotTestCase : NSObject
+@end
+@implementation PodsDummy_Pods_HTPressableButtonTests_FBSnapshotTestCase
+@end
